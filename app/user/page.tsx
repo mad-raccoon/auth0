3563d02769d38c1c309f607a2e2e036b2feb2a1a
rui@trevor.io <rui@embeddable.com>
2023-10-25 15:46:35 +0100

    test

---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import ProfileClient from "../ProfileClient";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <a href="/api/auth/login">Login</a>
+      <a href="/api/auth/logout">Logout</a>
+      <ProfileClient />
+    </main>
+  );
+}
