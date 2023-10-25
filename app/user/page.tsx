ee1f1e6635fc2a5bc8b7b2453440391996e75fdc
rui@trevor.io <rui@embeddable.com>
2023-10-25 15:48:19 +0100

    test

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import ProfileClient from "../ProfileClient";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <a href="/api/auth/login">Login</a>
       <a href="/api/auth/logout">Logout</a>
       <a href="/">Home</a>
